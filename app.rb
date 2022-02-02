6a484c50eae1f275f193155d936f163e09dff27d
shalini9 <shalini.nangia1@gmail.com>
2022-02-02 15:14:24 +0000

    entering players

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # get '/' do
+  #   'Hello Battle!'
+  #   'Testing infrastructure working!'
+  # end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
+
