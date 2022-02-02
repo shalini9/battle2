fc80c321e295e074d61d55832617d324a8f701be
shalini9 <shalini.nangia1@gmail.com>
2022-02-02 15:57:14 +0000

    finished post redirect GET pattern

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,9 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Charlotte'
     fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
+
+    save_and_open_page # will save the web page and open the browser to display it
+
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
