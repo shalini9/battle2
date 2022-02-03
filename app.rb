a16daf9a2444126812ab7da95ae89d0cd66c2bc0
shalini9 <shalini.nangia1@gmail.com>
2022-02-03 16:35:05 +0000

    still working on reducing points

--- app.rb
+++ app.rb
@@ -23,24 +23,27 @@ class Battle < Sinatra::Base
     # @player_2_name = params[:player_2_name]
     # session[:player_1_name] = params[:player_1_name]
     # session[:player_2_name] = params[:player_2_name]
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
     redirect '/play'
   end
 
   get '/play' do
     # @player_1_name = session[:player_1_name]
     # @player_2_name = session[:player_2_name]
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :play
   end 
 
   get '/attack' do
     # @player_1_name = session[:player_1_name]
     # @player_2_name = session[:player_2_name]
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    # @player_1_name = $player_1.name
+    # @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
 
